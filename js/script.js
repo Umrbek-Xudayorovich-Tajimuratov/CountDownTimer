9efddab5d897670c94ef264bb11c76e18b963a79
Umrbek-Xudayorovich-Tajimuratov <78151859+Umrbek-Xudayorovich-Tajimuratov@users.noreply.github.com>
2021-05-17 13:41:16 +0500

    User can input time and Title

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-const mybirthday = "18 Oct 2021";
+// const mybirthday = new Date("18 Oct 2021");
+
+const setTitle = document.getElementById("settitle");
+const stateDate = document.getElementById("state");
 
 const dayEl = document.getElementById("day");
 const hourEl = document.getElementById("hour");
@@ -6,13 +9,13 @@ const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
 
-function countDown(){
-    const myBirthday = new Date(mybirthday);
-    const currentDate = new Date();
 
+function countDown(){
+    const currentDate = new Date();
+    const myBirthday = checkData();
+    
     const totalSeconds  = (myBirthday - currentDate) / 1000;
 
-    console.log(myBirthday - currentDate);
     const days = Math.floor(totalSeconds/24/3600);
     const hours = Math.floor(totalSeconds/3600) % 24;
     const minutes = Math.floor(totalSeconds/60) % 24 % 60;
@@ -29,4 +32,23 @@ setInterval(countDown, 1000);
 
 function formatTime(time){
     return (time < 10) ? `0${time}`: time;
+}
+
+
+function checkData(){
+    let setDate = new Date(document.getElementById("date-input").value);
+    const currentDateCheck = new Date();
+    stateDate.innerHTML = "Invalid Date. Must be future target!";
+
+    if(setDate == "Invalid Date" || setDate <= currentDateCheck) {
+        setDate = new Date("18 Oct 2021");
+        return setDate;
+     }
+    else{
+        document.getElementById("titleh1").innerHTML = (!setTitle.value)? "My Birthday!": setTitle.value;
+        stateDate.innerHTML = "";
+        return setDate;
+    }
+
+
 }
